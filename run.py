d4871f9b1b51aedb285808474ce502ccda292805
ahitagnied <ahitagnis@gmail.com>
2025-05-28 20:36:26 -0500

    update for multiple view

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.append(current_dir)
 
-from scripts.single_obj import load_config, render_obj
+from scripts.multiple_obj import load_config, render_obj
 
 def main():
     # get the absolute path to the config file
-    config_path = os.path.join(current_dir, "configs", "single_obj.yaml")
+    config_path = os.path.join(current_dir, "configs", "multiple_obj.yaml")
     
     # print status for debugging
     print(f"looking for config at: {config_path}")
