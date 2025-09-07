619e077d6c00fcfefc266e8cf455f544e42e57e8
ahitagnied <ahitagnis@gmail.com>
2025-09-06 22:23:00 -0500

    fix: tune params

--- gen/objs/deformed_mirror.py
+++ gen/objs/deformed_mirror.py
@@ -9,10 +9,10 @@ NOISE_OCTAVES = 4
 
 MIRROR_RESOLUTION = 512
 MIRROR_SIZE = 10.0  # -mirror_size to +mirror_size
-DEFORM_SCALE = 0.12
+DEFORM_SCALE = 0.15
 OUTPUT_FILENAME = "deformed_surface.obj"
 
-SMOOTH_SCALE = 16.0
+SMOOTH_SCALE = 17.0
 SMOOTH_OCTAVES = 5
 
 def generate_perlin_noise(width=NOISE_WIDTH, height=NOISE_HEIGHT, scale=NOISE_SCALE, octaves=NOISE_OCTAVES):
